63e82306cd1bc689bede8a2131c4be19dafc8f39
구연재 <yeonjaekoo@guyeonjaeui-MacBookPro.local>
2025-05-02 16:33:06 +0900

    BrowserRouter basename 수정: PUBLIC_URL 또는 '/' 사용

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; 
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n/i18n';
 import App from './App';
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
+    <BrowserRouter basename={process.env.PUBLIC_URL || '/'}>
       <App />
+      </BrowserRouter>
     </I18nextProvider>
   </React.StrictMode>
 );
