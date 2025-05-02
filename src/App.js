c8c974da7836237f70563adcaf85a4c89e2cc3b3
구연재 <yeonjaekoo@guyeonjaeui-MacBookPro.local>
2025-05-02 16:09:11 +0900

    라우터 설정반영

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';    // BrowserRouter 는 index.js 에서 이미 감쌌습니다
 import { useTranslation } from 'react-i18next';
 import './i18n/i18n';
+
 import Header from './components/Header/Header';
 import Introduction from './components/Introduction/Introduction';
 import MainWindow from './components/MainWindow/MainWindow';
@@ -11,44 +12,44 @@ import './App.css';
 
 function App() {
   const { i18n } = useTranslation();
-  function changeLanguage(lng) { i18n.changeLanguage(lng); }
+  const changeLanguage = (lng) => i18n.changeLanguage(lng);
 
   return (
-    <Router>
-      <div className="app">
-        <Header />
-        <div className="background-title">
+    <div className="app">
+      <Header />
+
+      <div className="background-title">
         <span className="title-jaes">Jae’s</span>
         <span className="title-archive">Archive</span>
       </div>
 
-        {/* 배경 레이어 */}
-        <div className="layer-flares" />
-        <div className="layer-bubbles" />
-        <div className="layer-water" />
-        <div className="sealife sealife-1" />
-        <div className="sealife sealife-11" />
-        <div className="sealife sealife-13" />
-        <div className="sealife sealife-27" />
+      {/* 배경 레이어들 */}
+      <div className="layer-flares" />
+      <div className="layer-bubbles" />
+      <div className="layer-water" />
+      <div className="sealife sealife-1" />
+      <div className="sealife sealife-11" />
+      <div className="sealife sealife-13" />
+      <div className="sealife sealife-27" />
 
-        {/* 라우트 정의 */}
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div className="content-wrapper">
-                <div className="main">
-                  <Introduction />
-                  <MainWindow />
-                  <Sidebar />
-                </div>
-                <LanguageSwitch changeLanguage={changeLanguage} />
+      {/* 경로별 렌더링 */}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="content-wrapper">
+              <div className="main">
+                <Introduction />
+                <MainWindow />
+                <Sidebar />
               </div>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+              <LanguageSwitch changeLanguage={changeLanguage} />
+            </div>
+          }
+        />
+        {/* 예: /gallery 나 /about 같은 추가 Route 가 필요하면 여기에 나열 */}
+      </Routes>
+    </div>
   );
 }
 
